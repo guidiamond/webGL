db33cb77e42a5904ac4d5d36d388b1f79151554d
guidiamond <guilherme.batista770@gmail.com>
2020-11-02 13:55:47 -0300

    shaders with string expansion (cleaner)

--- js/shaders.js
+++ js/shaders.js
@@ -1,24 +1,27 @@
-const vertexShaderText = [
-  "precision mediump float;",
-  "",
-  "attribute vec3 vertPosition;",
-  "attribute vec3 vertColor;",
-  "varying vec3 fragColor;",
-  "uniform mat4 mWorld;", // rotating the piramid in 3D space
-  "uniform mat4 mView;", // where the camera is sitting at
-  "uniform mat4 mProj;",
-  "",
-  "void main() {",
-  "  fragColor = vertColor;",
-  "  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);",
-  "}",
-].join("\n");
+// Each time a vertex shader is executed
+// the next value from each specified buffer
+// is pulled out and assigned to an attribute.
+const vertexShaderSource = `
+    precision mediump float;
 
-const fragmentShaderText = [
-  "precision mediump float;",
-  "varying vec3 fragColor;",
-  "",
-  "void main() {",
-  "  gl_FragColor = vec4(fragColor, 1.0);", // rgba format
-  "}",
-].join("\n");
+    attribute vec3 vertPosition; // inputs that come from buffer
+    attribute vec3 vertColor; // inputs that come from buffer
+    varying vec3 fragColor; // value that can be accessed by the frag shader (interpolated while executing)
+    uniform mat4 mWorld; // rotating the piramid in 3D space (global variable)
+    uniform mat4 mView; // where the camera is sitting at (global variable)
+    uniform mat4 mProj; // (global variable)
+
+    void main() {
+      fragColor = vertColor;
+      gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
+    }
+  `;
+
+const fragmentShaderSource = `
+    precision mediump float;
+    varying vec3 fragColor;
+
+    void main() {
+      gl_FragColor = vec4(fragColor, 1.0); // rgba format
+    }
+  `;
